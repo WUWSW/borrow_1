097abd01c5dbda8211fe128faeeef3c7d4b4f925
WUWSW <159779029+WUWSW@users.noreply.github.com>
2025-10-24 21:50:30 +0700

    register v.1

---
+++ lib/login/register.dart
@@ -0,0 +1,260 @@
+import 'package:flutter/material.dart';
+
+class Register extends StatefulWidget {
+  const Register({super.key});
+
+  @override
+  State<Register> createState() => _RegisterState();
+}
+
+class _RegisterState extends State<Register> {
+  final _formKey = GlobalKey<FormState>();
+  final _usernameController = TextEditingController();
+  final _passwordController = TextEditingController();
+  final _confirmPasswordController = TextEditingController();
+  bool _obscureText = true;
+  bool _obscureConfirmText = true;
+
+  void _togglePasswordVisibility() {
+    setState(() => _obscureText = !_obscureText);
+  }
+
+  void _toggleConfirmPasswordVisibility() {
+    setState(() => _obscureConfirmText = !_obscureConfirmText);
+  }
+
+  void _handleRegister() {
+    if (_formKey.currentState!.validate()) {
+      final username = _usernameController.text.trim();
+      final password = _passwordController.text.trim();
+
+      // TODO: connect to backend here (e.g. via API or MySQL)
+      // ตัวอย่าง mock การสมัคร
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text('Register Successful! Welcome $username 🎉'),
+          backgroundColor: Colors.green,
+        ),
+      );
+
+      // กลับไปหน้า Login หลังสมัครสำเร็จ
+      Navigator.pop(context);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final screenHeight = MediaQuery.of(context).size.height;
+    final screenWidth = MediaQuery.of(context).size.width;
+
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: SingleChildScrollView(
+          padding: EdgeInsets.symmetric(horizontal: screenWidth * 0.1),
+          child: Form(
+            key: _formKey,
+            child: Column(
+              children: [
+                Image.asset('img/dice.png', width: screenWidth * 0.2),
+                SizedBox(height: screenHeight * 0.02),
+
+                Text(
+                  'REGISTER',
+                  style: TextStyle(
+                    fontSize: screenWidth * 0.07,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.deepOrange,
+                  ),
+                ),
+                Text(
+                  'TO BOARD GAME SS',
+                  style: TextStyle(
+                    fontSize: screenWidth * 0.03,
+                    color: Colors.deepOrange.shade300,
+                  ),
+                ),
+                SizedBox(height: screenHeight * 0.04),
+
+                // Username field
+                TextFormField(
+                  controller: _usernameController,
+                  decoration: InputDecoration(
+                    hintText: 'username',
+                    hintStyle: TextStyle(color: Colors.red.shade400),
+                    contentPadding: EdgeInsets.symmetric(
+                      vertical: screenHeight * 0.015,
+                      horizontal: screenWidth * 0.05,
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(30),
+                      borderSide: BorderSide(
+                        color: Colors.orange.shade200,
+                        width: 1.5,
+                      ),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(30),
+                      borderSide: const BorderSide(
+                        color: Colors.deepOrange,
+                        width: 2,
+                      ),
+                    ),
+                  ),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter a username';
+                    }
+                    return null;
+                  },
+                ),
+                SizedBox(height: screenHeight * 0.015),
+
+                // Password field
+                TextFormField(
+                  controller: _passwordController,
+                  obscureText: _obscureText,
+                  decoration: InputDecoration(
+                    hintText: 'password',
+                    hintStyle: TextStyle(color: Colors.red.shade400),
+                    suffixIcon: IconButton(
+                      icon: Icon(
+                        _obscureText ? Icons.visibility : Icons.visibility_off,
+                        color: Colors.red.shade400,
+                        size: screenWidth * 0.05,
+                      ),
+                      onPressed: _togglePasswordVisibility,
+                    ),
+                    contentPadding: EdgeInsets.symmetric(
+                      vertical: screenHeight * 0.015,
+                      horizontal: screenWidth * 0.05,
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(30),
+                      borderSide: BorderSide(
+                        color: Colors.orange.shade200,
+                        width: 1.5,
+                      ),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(30),
+                      borderSide: const BorderSide(
+                        color: Colors.deepOrange,
+                        width: 2,
+                      ),
+                    ),
+                  ),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter a password';
+                    } else if (value.length < 6) {
+                      return 'Password must be at least 6 characters';
+                    }
+                    return null;
+                  },
+                ),
+                SizedBox(height: screenHeight * 0.015),
+
+                // Confirm Password field
+                TextFormField(
+                  controller: _confirmPasswordController,
+                  obscureText: _obscureConfirmText,
+                  decoration: InputDecoration(
+                    hintText: 'confirm password',
+                    hintStyle: TextStyle(color: Colors.red.shade400),
+                    suffixIcon: IconButton(
+                      icon: Icon(
+                        _obscureConfirmText
+                            ? Icons.visibility
+                            : Icons.visibility_off,
+                        color: Colors.red.shade400,
+                        size: screenWidth * 0.05,
+                      ),
+                      onPressed: _toggleConfirmPasswordVisibility,
+                    ),
+                    contentPadding: EdgeInsets.symmetric(
+                      vertical: screenHeight * 0.015,
+                      horizontal: screenWidth * 0.05,
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(30),
+                      borderSide: BorderSide(
+                        color: Colors.orange.shade200,
+                        width: 1.5,
+                      ),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(30),
+                      borderSide: const BorderSide(
+                        color: Colors.deepOrange,
+                        width: 2,
+                      ),
+                    ),
+                  ),
+                  validator: (value) {
+                    if (value != _passwordController.text) {
+                      return 'Passwords do not match';
+                    }
+                    return null;
+                  },
+                ),
+                SizedBox(height: screenHeight * 0.02),
+
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: TextButton(
+                    onPressed: () {
+                      Navigator.pop(context); // กลับไปหน้า Login
+                    },
+                    style: TextButton.styleFrom(
+                      padding: EdgeInsets.zero,
+                      minimumSize: Size(0, 0),
+                      tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+                    ),
+                    child: const Text(
+                      'Login',
+                      style: TextStyle(
+                        color: Colors.deepOrange,
+                        fontWeight: FontWeight.w600,
+                        fontSize: 13,
+                      ),
+                    ),
+                  ),
+                ),
+                
+
+                SizedBox(height: screenHeight * 0.03),
+
+                SizedBox(
+                  width: 130,
+                  height: screenHeight * 0.06,
+                  child: ElevatedButton(
+                    onPressed: _handleRegister,
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.white,
+                      foregroundColor: Colors.deepOrange,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(30),
+                        side: const BorderSide(
+                          color: Colors.deepOrange,
+                          width: 1.5,
+                        ),
+                      ),
+                    ),
+                    child: Text(
+                      'REGISTER',
+                      style: TextStyle(
+                        fontSize: screenWidth * 0.04,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
